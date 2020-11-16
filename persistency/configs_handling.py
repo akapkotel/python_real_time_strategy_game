bfeffeffbdcb80d0a9b8c02fb1eab4987fecf994
akapkotel <btcuserbtc@gmail.com>
2020-11-17 00:27:32 +0100

    1. Added CSV config files handling - configs_handling.py. 2. Moved objects spawning to new class: ObjectsSpawner in spawning.py 3. Prettier menu-buttons. 4. Started implementing attacking of enemies logic.

--- persistency/configs_handling.py
+++ persistency/configs_handling.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python
 import csv
 
-from typing import Dict
+from typing import Dict, Union, Tuple, List
 
 from utils.functions import get_path_to_file, log
 
 
 def read_csv_files():
     configs = {'units': {}, 'buildings': {}, 'technologies': {}}
-
     for key in configs:
         try:
             configs[key].update(read_single_file(f'{key}.csv'))
         except Exception as e:
             log(f'{str(e)}')
-
     print(configs)
     return configs
 
@@ -23,17 +21,33 @@ def read_single_file(filename: str):
     category_dict = {}
     with open(get_path_to_file(filename), newline='') as file:
         for row in csv.DictReader(file):
-            category_dict[row['object_name']] = convert_csv_numeric_data(row)
-
+            category_dict[row['object_name']] = convert_csv_data(row)
     return category_dict
 
 
-def convert_csv_numeric_data(row) -> Dict:
-    """Identify numeric values and convert them to int or float."""
+def convert_csv_data(row) -> Dict:
+    """
+    Read values from CSV file, unpack them from strings, convert to numeric
+    types if required and assign as dict values.
+    """
     converted = {}
     for key, value in row.items():
-        if value[0].isnumeric():
-            converted[key] = float(value) if '.' in value else int(value)
+        if value.startswith(('(', '[')):
+            converted[key] = unpack_value(value, value[0])
+        elif value[0].isnumeric():
+            converted[key] = convert_to_numeric(value)
         else:
             converted[key] = value
+        print(converted[key])
     return converted
+
+
+def unpack_value(value: str, bracket: str) -> Union[Tuple, List]:
+    """Convert str representation of tuple or list to python tuple or list."""
+    values = [convert_to_numeric(v) for v in value.strip('([)]').split(';')]
+    return tuple(values) if bracket == '(' else values
+
+
+def convert_to_numeric(value: str) -> Union[float, int]:
+    """Identify numeric values and convert them to int or float."""
+    return float(value) if '.' in value else int(value)
